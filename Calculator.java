d24773ae6dacc5bd7f5480c1a786c0dd3dbde0b9
Eya-ben-nhila <eyabenhila@gmail.com>
2025-02-21 13:35:37 +0100

    Added Calculator.java file

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+package Calculator;
+
+public class Calculator {
+	double a,b;
+	
+	double add(double a,double b) {
+		return a+b;}
+	double sub(double a,double b) {
+		return a-b;}
+	double mul(double a,double b) {
+		return a*b;}
+	double div(double a,double b) {
+		return a/b;}
+}
